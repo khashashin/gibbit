18d76d15cb1c7044bad20696ed398d2cc1ef602c
Christen Janis <bchrij@bbcag.ch>
2020-10-27 15:45:33 +0100

    Finish home page except for links

--- templates/default/index.php
+++ templates/default/index.php
@@ -1,1 +1,20 @@
-Hallo Welt!
+<!-- Breadcrumb -->
+<div class="row">
+    <p>Home ></p>
+</div>
+<div class="row">
+    <div class="hero-section text-center">
+        <h1>gibbit - Ein Forum für alle</h1>
+        <h2>Hier kannst du dich mit anderen Nutzern austauschen</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="home-main-content text-center">
+        <h3>Fang an Gibbit zu durchstöbern:</h3>
+        <a href="/post"><button class="home-latest-posts-button">Neueste Posts</button></a>
+    </div>
+</div>
+
+</main>
+</body>
+</html>
