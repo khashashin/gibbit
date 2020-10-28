fda31abd65ddbf866abb038117fd92f9a2aa1260
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-28 09:23:25 +0100

    created post index

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,18 @@
 namespace App\Controller;
 
 
+use App\Repository\PostRepository;
+use App\View\View;
+
 class PostController
 {
+    public function index() {
+        $postRepository = new PostRepository();
 
+        $view = new View('post/index');
+        $view->title = 'Posts';
+        $view->heading = 'Posts';
+        $view->posts = $postRepository->readAll();
+        $view->display();
+    }
 }
--- templates/post/index.php
+++ templates/post/index.php
@@ -1,1 +1,13 @@
-<?php
+<article class="hreview open special">
+    <?php if (empty($posts)): ?>
+        <div class="dhd">
+            <h2 class="item title">Hoopla! Keine User gefunden.</h2>
+        </div>
+    <?php else: ?>
+        <?php foreach ($posts as $post): ?>
+            <ul>
+                <li><a href=""></a><?= $post->title; ?></li>
+            </ul>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</article>
