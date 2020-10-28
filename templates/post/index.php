5ac63c94e8b36a8e042f4b7473b0435ab7818e15
Janis Christen <janis.christen@sunrise.ch>
2020-10-28 14:57:42 +0100

    Style post overview

--- templates/post/index.php
+++ templates/post/index.php
@@ -1,13 +1,36 @@
 <article class="hreview open special">
     <?php if (empty($posts)): ?>
         <div class="dhd">
-            <h2 class="item title">Hoopla! Keine User gefunden.</h2>
+            <h2 class="item title">Leider wurden bisher keine Posts erstellt.</h2>
         </div>
     <?php else: ?>
+    <h1>Neuste Posts</h1>
         <?php foreach ($posts as $post): ?>
-            <ul>
-                <li><a href="/post/details?id=<?= $post->id?>"><?= $post->title; ?></a></li>
-            </ul>
+        <?php
+            // Zeitformat umwandeln
+            $created_at = DateTime::createFromFormat('Y-m-d H:i:s', $post->created_at);
+            $created_at = date_format($created_at, 'd.m.Y H:i');
+
+            // Autor Name evaluieren
+            $user_id = $post->user_id;
+            $userRepository = new \App\Repository\UserRepository();
+            $user_name = $userRepository->readById($user_id)->username;
+
+            // Text kürzen
+            $text = $post->text;
+            if(strlen($text) > 100) {
+                // Text kürzen, da er über 120 Zeichen lang ist
+                $text = substr($text, 0, 120);
+                $text .= '...';
+            }
+        ?>
+
+        <div class="post">
+            <a href="/post/details?id=<?= $post->id?>" class="post-title-link"><h3 class="post-title"><?= $post->title; ?></h3></a>
+            <p class="post-content"><?= $text ?></p>
+            <p class="created_at">veröffentlicht am <?= $created_at ?></p>
+            <p class="author">Autor: <?= $user_name ?></p>
+        </div>
         <?php endforeach; ?>
     <?php endif; ?>
 </article>
