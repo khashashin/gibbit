ab3feaaba16201cb46fb674090bb893914e6addc
Christen Janis <bchrij@bbcag.ch>
2020-10-28 08:24:22 +0100

    Raw footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,26 @@
-    </main>
+</main>
 
-    <footer class="footer">
-      <div class="container">
-        <span class="text-muted">&copy; ICT Berufsbildungcenter AG 2016</span>
-      </div>
-    </footer>
+<footer class="footer">
+    <div class="container">
+        <div class="footer-content">
+            <span class="text-muted footer-title">gibbit</span>
+            <ul class="footer-links">
+                <li><i class="fa fa-github"></i> GitHub</li>
+                <li><i class="fa fa-window-maximize"></i> Website</li>
+                <li><i class="fa fa-twitter"></i> Twitter</li>
+            </ul>
+        </div>
+    </div>
+</footer>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
 </html>
