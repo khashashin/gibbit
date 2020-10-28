da0d190df4a609386d0d645bd970be73e17cf979
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-28 09:22:30 +0100

    Created post index, controller, repository

---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Repository;
+
+/**
+ * Das PostRepository ist zuständig für alle Zugriffe auf die Tabelle "post".
+ *
+ * Die Ausführliche Dokumentation zu Repositories findest du in der Repository Klasse.
+ */
+class PostRepository extends Repository
+{
+    protected $tableName = 'post';
+
+}
