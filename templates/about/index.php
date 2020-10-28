2ee88a126c7c0780f5a482fbafc552e7b826cab0
Janis Christen <janis.christen@sunrise.ch>
2020-10-28 14:22:45 +0100

    Add about page (BE+FE)

---
+++ templates/about/index.php
@@ -0,0 +1,39 @@
+<div class="about-us">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <div class="container">
+                    <h2>Yusuf Khasbulatov</h2>
+                    <p class="title">Entwickler</p>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                    <p>bkhasy@bbcag.ch</p>
+                    <a href="mailto:bkashy@bbcag.ch"><button class="btn btn-primary">Kontaktieren</button></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="container">
+                    <h2>Franz Kilchenmann</h2>
+                    <p class="title">Entwickler</p>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                    <p>bkilcf@bbcag.ch</p>
+                    <a href="mailto:bkilcf@bbcag.ch"><button class="btn btn-primary">Kontaktieren</button></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <div class="container">
+                    <h2>Janis Christen</h2>
+                    <p class="title">Entwickler</p>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                    <p>bchrij@bbcag.ch</p>
+                    <a href="mailto:bchrij@bbcag.ch"><button class="btn btn-primary">Kontaktieren</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
