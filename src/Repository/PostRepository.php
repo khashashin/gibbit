c1d64ddf00147413e4a353606dd3cd73e199cc24
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-30 00:34:18 +0100

    implemented post repository

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -3,6 +3,9 @@
 
 namespace App\Repository;
 
+use App\Database\ConnectionHandler;
+use Exception;
+
 /**
  * Das PostRepository ist zuständig für alle Zugriffe auf die Tabelle "post".
  *
@@ -11,5 +14,44 @@ namespace App\Repository;
 class PostRepository extends Repository
 {
     protected $tableName = 'post';
+
+    /**
+     * Erstellt einen neuen Post mit den gegebenen Werten.
+     *
+     * @param int $user_id Wert für die Spalte title
+     * @param string $title Wert für die Spalte title
+     * @param string $text Wert für die Spalte text
+     *
+     * @throws Exception falls das Ausführen des Statements fehlschlägt
+     *
+     * @return int Das id von neu kreierte Post
+     */
+    public function create(int $user_id, string $title, string $text) {
+        // Eingabe validieren
+        if(isset($user_id) && isset($title) && isset($text)) {
+            if(!empty($user_id) && !empty($title) && !empty($text)) {
+                // Verhindert XSS
+                htmlspecialchars($user_id);
+                htmlspecialchars($title);
+                htmlspecialchars($text);
+            } else {
+                header('/post/create?error=Bitte lasse keine Eingabe leer'); // Mit Fehler returnen, dass Werte leer waren
+            }
+        } else {
+            header('/post/create?error=Bitte gib überall einen Wert an'); // Mit Fehler returnen, dass Werte fehlen
+        }
+
+        $query = "INSERT INTO $this->tableName (user_id, title, text, created_at, is_approved) VALUES (?, ?, ?, ?, ?)";
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+
+        $statement->bind_param('isssi', $user_id,$title, $text, date("c"), 1);
+
+        if (!$statement->execute()) {
+            throw new Exception($statement->error);
+        } else {
+            header('Location: /post/index');
+        }
+
+    }
 
 }
