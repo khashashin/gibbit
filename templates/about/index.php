e1965b28dd4d218e1add5873c15c55d7085318a3
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-31 21:09:57 +0100

    removed stylesheet related to about us page

--- templates/about/index.php
+++ templates/about/index.php
@@ -3,11 +3,11 @@
         <div class="col-12">
             <div class="card my-3">
                 <div class="card-body">
-                    <h2 class="card-title">Yusuf Khasbulatov</h2>
+                    <h2 class="card-title">Janis Christen</h2>
                     <p class="card-subtitle mb-2 text-muted">Entwickler</p>
                     <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-                    <a href="mailto:bkashy@bbcag.ch" class="card-link">bkhasy@bbcag.ch</a>
-                    <a href="https://khas.dev" class="card-link" target="_blank">Website</a>
+                    <a href="mailto:bchrij@bbcag.ch" class="card-link">bchrij@bbcag.ch</a>
+                    <a href="https://gibbit.ch" class="card-link" target="_blank">Website</a>
                 </div>
             </div>
             <div class="card my-3">
@@ -21,11 +21,11 @@
             </div>
             <div class="card my-3">
                 <div class="card-body">
-                    <h2 class="card-title">Janis Christen</h2>
+                    <h2 class="card-title">Yusuf Khasbulatov</h2>
                     <p class="card-subtitle mb-2 text-muted">Entwickler</p>
                     <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-                    <a href="mailto:bchrij@bbcag.ch" class="card-link">bchrij@bbcag.ch</a>
-                    <a href="https://gibbit.ch" class="card-link" target="_blank">Website</a>
+                    <a href="mailto:bkashy@bbcag.ch" class="card-link">bkhasy@bbcag.ch</a>
+                    <a href="https://khas.dev" class="card-link" target="_blank">Website</a>
                 </div>
             </div>
         </div>
