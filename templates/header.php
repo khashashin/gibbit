6d3a66097811022e1d8641dc915136b3ff4bf388
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-31 17:05:09 +0100

    added breadcrumbs

--- templates/header.php
+++ templates/header.php
@@ -62,5 +62,53 @@
         </div>
     </nav>
 </header>
+<!-- Breadcrumb -->
+<!-- source: https://stackoverflow.com/a/2594624/7986808 -->
+<?php
+
+// This function will take $_SERVER['REQUEST_URI'] and build a breadcrumb based on the user's current path
+function breadcrumbs($home = 'Home') {
+    // This gets the REQUEST_URI (/path/to/file.php), splits the string (using '/') into an array, and then filters out any empty values
+    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+
+    // This will build our "base URL" ... Also accounts for HTTPS :)
+    $base = (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+
+    // Initialize a temporary array with our breadcrumbs. (starting with our home page, which I'm assuming will be the base URL)
+    $breadcrumbs = Array("<a href=\"$base\">$home</a>");
+
+    // Find out the index for the last value in our path array
+    $array = array_keys($path);
+    $last = end($array);
+
+    // Build the rest of the breadcrumbs
+    foreach ($path AS $x => $crumb) {
+        // Our "title" is the text that will be displayed (strip out .php and turn '_' into a space)
+        $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
+        // Workaround to create plural form of a title
+        if (substr($title, -1) != "s") {
+            $title = $title . "s";
+        }
+        // If we are not on the last index, then display an <a> tag
+        if ($x != $last)
+            $breadcrumbs[] = "<a href=\"$base$crumb\">$title</a>";
+        // Otherwise, just display the title (minus)
+        else
+            $breadcrumbs[] = $title;
+    }
+
+    // Build our temporary array (pieces of bread) into one big string :)
+    return $breadcrumbs;
+}
+
+?>
+<nav aria-label="breadcrumb" class="container mt-5 pt-5">
+    <?php $crumbs = breadcrumbs()?>
+    <ol class="breadcrumb">
+        <?php foreach ($crumbs as $crumb): ?>
+            <li class="breadcrumb-item" aria-current="page"><?= $crumb?></li>
+        <?php endforeach; ?>
+    </ol>
+</nav>
 
 <main class="container">
