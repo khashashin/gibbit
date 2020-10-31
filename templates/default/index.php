4b0ab5697fa8f45135f2ab61c56e378e8f8c069a
Yusuf Khasbulatov <khashashin@gmail.com>
2020-10-31 17:05:29 +0100

    changed hero section to bootstrap jumbotron

--- templates/default/index.php
+++ templates/default/index.php
@@ -1,18 +1,9 @@
-<!-- Breadcrumb -->
-<div class="row">
-    <p>Home ></p>
-</div>
-<div class="row">
-    <div class="hero-section text-center">
-        <h1>gibbit - Ein Forum für alle</h1>
-        <h2>Hier kannst du dich mit anderen Nutzern austauschen</h2>
-    </div>
-</div>
-<div class="row">
-    <div class="home-main-content text-center">
-        <h3>Fang an Gibbit zu durchstöbern:</h3>
-        <a href="/post"><button class="home-latest-posts-button">Neueste Posts</button></a>
-    </div>
+<div class="jumbotron">
+    <h1 class="display-4">gibbit - Ein Forum für alle</h1>
+    <p class="lead">Hier kannst du dich mit anderen Nutzern austauschen.</p>
+    <hr class="my-4">
+    <p>Fang an Gibbit zu durchstöbern:</p>
+    <a class="btn btn-primary btn-lg" href="/post" role="button">Neueste Posts</a>
 </div>
 
 </main>
