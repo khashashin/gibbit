ceff962c48607194651193ea1966b1674e49ddd3
Yusuf Khasbulatov <khashashin@gmail.com>
2020-11-03 11:47:49 +0100

    Changed all browser native validation to bootstrap validation

--- templates/post/edit.php
+++ templates/post/edit.php
@@ -7,15 +7,21 @@
                 <p><?= $_GET['error'] ?></p>
             </div>
         <?php endif; ?>
-        <form action="/post/doUpdate" method="post" class="col-12">
+        <form action="/post/doUpdate" method="post" class="col-12 needs-validation" novalidate>
             <div class="form-group">
                 <label for="post-title">Titel</label>
                 <input id="post-title" type="text" name="title" class="form-control" required
                        value="<?= $post->title ?>">
+                <div class="invalid-feedback">
+                    Titel ist noch nötig.
+                </div>
             </div>
             <div class="form-group">
                 <label for="post-text">Post</label>
                 <textarea id="post-text" name="text" rows="10" class="form-control" required><?= $post->text ?></textarea>
+                <div class="invalid-feedback">
+                    Post-text ist noch nötig.
+                </div>
             </div>
             <input type="hidden" name="post_id" value="<?= $post->id ?>">
             <button type="submit" class="btn btn-primary">Speichern</button>
--- templates/user/login.php
+++ templates/user/login.php
@@ -11,14 +11,20 @@
     <!-- Login Formular -->
     <h1 class="h3">Bei gibbit anmelden</h1>
     <div class="col-12 col-sm-6">
-        <form action="/user/doLogin" method="post">
+        <form action="/user/doLogin" method="post" class="needs-validation" novalidate>
             <div class="form-group">
                 <label for="username">Benutzername</label>
                 <input type="text" class="form-control" id="username" name="username" required>
+                <div class="invalid-feedback">
+                    Benutzername ist noch nötig.
+                </div>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" name="password" required>
+                <div class="invalid-feedback">
+                    Password ist noch nötig.
+                </div>
             </div>
             <button type="submit" class="btn btn-primary btn-block">Anmelden</button>
         </form>
