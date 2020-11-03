d25bb69d89719bc25660ab67ad598950197249b2
Janis Christen <janis.christen@sunrise.ch>
2020-11-03 11:09:44 +0100

    Add autocomplete=off to password fields

--- templates/user/login.php
+++ templates/user/login.php
@@ -18,7 +18,7 @@
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <input type="password" autocomplete="off" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary btn-block">Anmelden</button>
         </form>
